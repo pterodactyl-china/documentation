e9f9afb65c4d0e463b7684198f7b3d63415f775a
飒爽师叔 <43847794+vlssu@users.noreply.github.com>
2022-07-25 19:48:01 +0800

    润色

--- .vuepress/theme/Home.vue
+++ .vuepress/theme/Home.vue
@@ -5,7 +5,7 @@
                 <div class="text-center">
                     <div>
                         <img class="max-w-xl w-full inline-block" src="./banner_china_logo.png"
-                            alt="Pterodactyl">
+                            alt="Pterodactyl-China">
                     </div>
                     <div class="mt-4">
                         <!-- <a class="btn hidden md:inline-block" href="https://demo.pterodactyl.io" target="_blank" rel="nofollow noopener">Demo</a> -->
@@ -60,7 +60,7 @@
                     <div class="feature">
                         <span><i class="icon icon-expand"></i></span>
                         <h3>可扩展性</h3>
-                        <p>无论你是一家托管公司，还是下一个Hyplex，或者只是一些玩视频游戏的家伙，我们都能为你提供服务。</p>
+                        <p>无论你是一家托管公司，还是下一个Hyplex，或者只玩一些网络游戏的人，我们都能为你提供服务。</p>
                     </div>
                 </div>
             </div>
