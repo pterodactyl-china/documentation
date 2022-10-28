b7aac94f3c14dc58b600273dd47f8245c0eb5bd6
飒爽师叔 <43847794+vlssu@users.noreply.github.com>
2022-10-28 08:48:13 +0800

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -75,7 +75,7 @@ module.exports = {
             },
             {
                 text: '获取帮助',
-                link: 'https://kook.top/0Grsf5'
+                link: 'https://bbs.pterodactyl.top'
             },
             {
                 text: 'API',
