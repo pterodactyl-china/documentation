25264130f25c206861898a234ff0ab528480ead5
飒爽师叔 <43847794+vlssu@users.noreply.github.com>
2022-10-28 08:49:01 +0800

    Update Home.vue

--- .vuepress/theme/Home.vue
+++ .vuepress/theme/Home.vue
@@ -113,7 +113,7 @@
                 <p class="m-4">准备好在翼龙号上飞翔了吗？</p>
                 <div class="mt-4">
                     <router-link class="btn inline-block" to="/project/introduction.html">关于本项目</router-link>
-                    <a class="btn inline-block" href="https://kook.top/0Grsf5">KOOK</a>
+                    <a class="btn inline-block" href="https://bbs.pterodactyl.top">翼龙中国社区</a>
                 </div>
             </div>
         </div>
